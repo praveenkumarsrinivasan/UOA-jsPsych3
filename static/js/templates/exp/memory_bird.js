6d4c0c5ee29545df7c463c7f789cc9b84c223e35
Praveen Kumar Srinivasan <praveen.sxi@gmail.com>
2015-09-29 01:50:42 +0000

    time warning

--- static/js/templates/exp/memory_bird.js
+++ static/js/templates/exp/memory_bird.js
@@ -1,5 +1,7 @@
 var memory_bird_template = ' \
 <h4 class="text-muted">Please push any key on your keyboard</h4> \
 <br/> \
+<p>Please do not spend more than 10 seconds looking at the image</p> \
+<br/> \
 <img src="static/images/cliparts/training/<%- memory_bird_number %>.png" class="memory_image" alt=""/> \
 ';
--- static/js/templates/exp/memory_images.js
+++ static/js/templates/exp/memory_images.js
@@ -1,6 +1,8 @@
 var memory_images_template = ' \
 <h2>Please choose which image you thought you previously saw</h2> \
 <br/> \
+<p>Please do not spend more than 10 seconds looking at the image</p> \
+<br/> \
 <div class="row"> \
     <div class="col-sm-4"><p>Please push 1 on your keyboard</p></div> \
     <div class="col-sm-4"><p>Please push 2 on your keyboard</p></div> \
