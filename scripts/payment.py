ef78b08ad907f3e627f413685dab55770a06dedf
Praveen Kumar Srinivasan <praveen.sxi@gmail.com>
2015-10-02 23:23:20 +1300

    payment script

---
+++ scripts/payment.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2015 praveenkumarsrinivasan <praveen.sxi@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+import csv
+import re
+import sys
+
+
+def main(args):
+    commands = []
+    with open(args[0], 'rb') as csvfile:
+        reader = csv.DictReader(csvfile)
+        payment = {}
+        for row in reader:
+            script = './grantBonus.sh -workerid %s -amount %s -assignment %s -reason "Bonus Payment"'
+            pay = 0.20
+
+            match = re.search(r'Total Points\s+\: (\d+)', row['trial_stimulus'])
+            if match:
+                total = int(match.group(1))
+                if total >=5 and total <= 35:
+                    pay = 2.00
+                elif total >= 36 and total <=45:
+                    pay = 2.50
+                elif total >= 46:
+                    pay = 3.00
+                else:
+                    pay = 0.20
+                workerid = row['workerid']
+                assignment = row['assignmentid']
+                script = script % (workerid, pay, assignment)
+                commands.append(script)
+
+
+    with open(args[1], 'wb') as outfile:
+        for command in commands:
+            outfile.write(command + '\n')
+        outfile.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
