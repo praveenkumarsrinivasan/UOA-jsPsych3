0d58f181e32d2caf1e327e88bff42982a5701271
Praveen Kumar Srinivasan <praveen.sxi@gmail.com>
2015-10-03 06:09:56 +0000

    2-4th batch payment

---
+++ scripts/output/old/2nd batch/test extended.sh
@@ -0,0 +1,8 @@
+cd $MTURK_CMD_HOME/bin/
+./grantBonus.sh -workerid A1CF8UAT2GVF4R -amount 3.0 -assignment 3DEL4X4EL7W1AHX2F3345MKXQPKYXE -reason "Bonus Payment"
+./grantBonus.sh -workerid A20YCH1UOFGQZB -amount 3.0 -assignment 32ZKVD547GYTHBXDLMIWQVXSYLUB35 -reason "Bonus Payment"
+./grantBonus.sh -workerid A28NNO56HW1N3B -amount 3.0 -assignment 3VAR3R6G1QCZ12SS18IENW52L658OY -reason "Bonus Payment"
+./grantBonus.sh -workerid AHBZRUOTJTXAL -amount 2.5 -assignment 3P529IW9KZW0AWZIWU8S57IUD5MFLE -reason "Bonus Payment"
+./grantBonus.sh -workerid ALF14D78OROQY -amount 2.5 -assignment 336YQZE83WPSEHOEQUWYJ866OGQ5MF -reason "Bonus Payment"
+./grantBonus.sh -workerid AXN64KJPQ6EVC -amount 3.0 -assignment 3IHR8NYAM8CG31HR455GVKUNTP5P4P -reason "Bonus Payment"
+cd -
