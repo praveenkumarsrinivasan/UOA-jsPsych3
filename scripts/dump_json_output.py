f1ec77a5fbebef95813243235863ca2ea4b555c6
Praveen Kumar Srinivasan <praveen.sxi@gmail.com>
2015-10-06 23:42:17 +0000

    7th batch dump script

--- scripts/dump_json_output.py
+++ scripts/dump_json_output.py
@@ -18,7 +18,7 @@ host   = 'localhost'
 user   = 'uoa'
 passwd = 'uoapasswd'
 db     = 'cognitive_intelligence'
-table  = 't_exp7'
+table  = 't_exp8'
 
 conn = mdb.connect(host, user, passwd, db)
 cursor = conn.cursor()
