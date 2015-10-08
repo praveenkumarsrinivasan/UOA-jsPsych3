8a106fb9476ce33bfae8360c936156fba731787c
Praveen Kumar Srinivasan <praveen.sxi@gmail.com>
2015-10-08 02:26:43 +0000

    prep for 8th batch, payment script for $5

---
+++ scripts/output/old/7th batch/test.sh
@@ -0,0 +1,9 @@
+cd $MTURK_CMD_HOME/bin/
+./grantBonus.sh -workerid A10A25H4472NHN -amount 3.0 -assignment 38SKSKU7R28KKN18X2ED84VMUM7LIJ -reason "Bonus Payment"
+./grantBonus.sh -workerid A10PFZZSONQ18A -amount 2.5 -assignment 3KKG4CDWKJ9VBE1KNG9J9UU8PA794D -reason "Bonus Payment"
+./grantBonus.sh -workerid A1U434WAQQYUBB -amount 2.5 -assignment 3HHRAGRYX9GZF7B260OSNS0D62IO9F -reason "Bonus Payment"
+./grantBonus.sh -workerid A2JD3LQ4RH8PKD -amount 3.0 -assignment 3N1FSUEFL6B7E4E1ZWELVQDYGVE4DH -reason "Bonus Payment"
+./grantBonus.sh -workerid A30060SFJCEFKX -amount 3.0 -assignment 3NXNZ5RS1B8SU1J3PE7QC6EIGDK79K -reason "Bonus Payment"
+./grantBonus.sh -workerid AACRJGY60XZMI -amount 3.0 -assignment 3E47SOBEYR7RHJ72KPLE5L5Z0ESCIV -reason "Bonus Payment"
+./grantBonus.sh -workerid AXGQEL7A01BIK -amount 3.0 -assignment 3UN61F00HX0JBX16V2SF570GVTK5RF -reason "Bonus Payment"
+cd -
