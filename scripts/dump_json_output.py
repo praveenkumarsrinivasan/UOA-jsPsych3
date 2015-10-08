4bc99b3a01a79e0259f49d2fcfa35e80801b52ea
Praveen Kumar Srinivasan <praveen.sxi@gmail.com>
2015-10-08 21:36:37 +0000

    8th batch result

--- scripts/dump_json_output.py
+++ scripts/dump_json_output.py
@@ -18,7 +18,7 @@ host   = 'localhost'
 user   = 'uoa'
 passwd = 'uoapasswd'
 db     = 'cognitive_intelligence'
-table  = 't_exp8'
+table  = 't_exp9'
 
 conn = mdb.connect(host, user, passwd, db)
 cursor = conn.cursor()
---
+++ scripts/output/old/8th/test.sh
@@ -0,0 +1,10 @@
+cd $MTURK_CMD_HOME/bin/
+./grantBonus.sh -workerid A13FC4N2ITJ6W9 -amount 5.0 -assignment 323Q6SJS8JRYO0GS59TUDFPNXG8FHN -reason "Bonus Payment"
+./grantBonus.sh -workerid A1C0O3YMKCP2V0 -amount 4.5 -assignment 3VW04L3ZLUHCAC7SA7H2M8DOPBNXXP -reason "Bonus Payment"
+./grantBonus.sh -workerid A234JVZIFPLPRT -amount 5.0 -assignment 3KB8R4ZV1FIUBN91G1PG9PM13I4BG3 -reason "Bonus Payment"
+./grantBonus.sh -workerid A2C8O1OQPL519P -amount 4.0 -assignment 3LEP4MGT3HBRZ3ZAEBO834J4QQNDBU -reason "Bonus Payment"
+./grantBonus.sh -workerid A2FO3XJQO7AH7Q -amount 4.5 -assignment 3KMS4QQVK31PQQH0BUWRQ75FPXCFKR -reason "Bonus Payment"
+./grantBonus.sh -workerid A2X6JMFO452B1 -amount 5.0 -assignment 3L4D84MIL03ET22OOZKFSDHFQNQJHV -reason "Bonus Payment"
+./grantBonus.sh -workerid A3FNFXUKLDGKBW -amount 5.0 -assignment 36U2A8VAG2AVQHYVGKOFFSCVV82KYT -reason "Bonus Payment"
+./grantBonus.sh -workerid A8XGT54G5L5HH -amount 5.0 -assignment 3I2PTA7R3U5MHF79PA7L269TWZLKQQ -reason "Bonus Payment"
+cd -
