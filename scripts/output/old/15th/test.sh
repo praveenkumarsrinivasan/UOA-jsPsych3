caf673c5d8380d623b908a0b0b3f6e96964f403e
Praveen Kumar Srinivasan <praveen.sxi@gmail.com>
2015-12-09 23:44:52 +0000

    test results

---
+++ scripts/output/old/15th/test.sh
@@ -0,0 +1,16 @@
+cd $MTURK_CMD_HOME/bin/
+./grantBonus.sh -workerid A152JSLCLZ4A0R -amount 0.2 -assignment 38YMOXR4MVAK2XI6ZFCQKCMXLIOW6Z -reason "Bonus Payment"
+./grantBonus.sh -workerid A1A3TGZ7DKJWRW -amount 0.2 -assignment 3QFUFYSY9ZQ4COSXQ29BJQGJUOR4FN -reason "Bonus Payment"
+./grantBonus.sh -workerid A1BGB5ZF4QVQQT -amount 2.5 -assignment 3GM6G9ZBKO8UZJZ4RQEWMOCK7OJMT7 -reason "Bonus Payment"
+./grantBonus.sh -workerid A1JPUM7KH3020K -amount 0.2 -assignment 3YDTZAI2WYRDM9YPJ3UQUOCPTE914K -reason "Bonus Payment"
+./grantBonus.sh -workerid A1RSB5JF68P265 -amount 0.2 -assignment 3Q5ZZ9ZEVPQDTSMAWTMFEQ5B7GA85G -reason "Bonus Payment"
+./grantBonus.sh -workerid A1XH05IKC77OXO -amount 0.2 -assignment 3RXCAC0YIS0B9PBUDP56INW0XPOG86 -reason "Bonus Payment"
+./grantBonus.sh -workerid A255IWUJ8XJFRA -amount 0.2 -assignment 320DUZ38G8X0T67D4T2X7D5ED0GGJO -reason "Bonus Payment"
+./grantBonus.sh -workerid A2CWE0TRHC3OE7 -amount 0.2 -assignment 3XUHV3NRVL96M3N3ZXGLR3YAAZZ5HR -reason "Bonus Payment"
+./grantBonus.sh -workerid A2TT6FAIWFD25N -amount 2.5 -assignment 3VAR3R6G1QCZ12SS18IENW52OBFO81 -reason "Bonus Payment"
+./grantBonus.sh -workerid A39QOA9M7GNF86 -amount 2.5 -assignment 3P1L2B7AD20UG9CBVXRIS1R88BCOLL -reason "Bonus Payment"
+./grantBonus.sh -workerid A3SCEZTWZYV3BC -amount 2.5 -assignment 30MVJZJNHNOLEWK5QNWNFWJR9NNJ92 -reason "Bonus Payment"
+./grantBonus.sh -workerid A40VIPEKG28VZ -amount 0.2 -assignment 3L70J4KAZHXMGTUI8XOC8TZYAL8ADU -reason "Bonus Payment"
+./grantBonus.sh -workerid A8SCDARA72QU5 -amount 2.0 -assignment 3RJSC4XJ115VB3H71P90LBZTOIX50P -reason "Bonus Payment"
+./grantBonus.sh -workerid AEOXYOJ3II8D8 -amount 2.0 -assignment 3MH9DQ757XN97MI7GS6BEFEPUGEUGY -reason "Bonus Payment"
+cd -
