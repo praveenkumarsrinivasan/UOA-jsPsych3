20c9b11a01455f2fcde141e1ba046eec9bfc8416
Praveen Kumar Srinivasan <praveen.sxi@gmail.com>
2016-03-14 18:37:34 +1300

    header template

--- static/js/templates/header.js
+++ static/js/templates/header.js
@@ -2,22 +2,23 @@ var header_template = ' \
 <nav class="navbar navbar-inverse navbar-fixed-top"> \
     <div class="container"> \
         <div class="navbar-header"> \
-            <a class="navbar-brand" href="#">The School of Psychology</a> \
+            <a class="navbar-brand">The University of Auckland</a> \
         </div> \
     </div> \
 </nav> \
  \
-<div class="jumbotron"> \
-    <div class="container"> \
-        <div id="header-text"> \
-            <img id="uniLogoBanner" src="static/images/uoa-logo-5.jpg" alt="UoA Logo" /> \
-            <h1>University of Auckland</h1> \
-            <!-- <h2><small> --> \
-            <p> \
-            Psychology Experiment \
-            </p> \
-            <!-- </small></h2> --> \
-        </div> \
-    </div> \
-</div> \
 ';
+//<div class="jumbotron"> \
+    //<div class="container"> \
+        //<div id="header-text"> \
+            //<img id="uniLogoBanner" src="static/images/uoa-logo-5.jpg" alt="UoA Logo" /> \
+            //<h1>University of Auckland</h1> \
+            //<!-- <h2><small> --> \
+            //<p> \
+            //Psychology Experiment \
+            //</p> \
+            //<!-- </small></h2> --> \
+        //</div> \
+    //</div> \
+//</div> \
+//';
